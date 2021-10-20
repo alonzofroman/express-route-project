7eba797ec4fdd659bfb6e52f92ec50667954dcc4
alonzofroman <alonzofroman@gmail.com>
2021-10-20 11:34:56 -0700

    Routes for diagnostics, 404

--- server.js
+++ server.js
@@ -27,6 +27,11 @@ app.get('/feedback', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
 );
 
+// Route for 404
+app.get('/404', (req, res) => 
+  res.sendFile(path.join(__dirname, '/public/pages/404.html'))
+);
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
